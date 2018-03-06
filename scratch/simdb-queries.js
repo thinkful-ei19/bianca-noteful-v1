cf20cd3dc47559d60ba3f3d0daa28b7ab72eada1
biancavee15 <vasquezbmarie@gmail.com>
2018-03-06 14:54:19 -0500

    get notes w/ search & get notes by ID

--- scratch/simdb-queries.js
+++ scratch/simdb-queries.js
@@ -5,24 +5,24 @@ const simDB = require('../db/simDB');
 const notes = simDB.initialize(data);
 
 // GET Notes with search
-notes.filter('cats', (err, list) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(list);
-});
+// notes.filter('cats', (err, list) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log(list);
+// });
 
-// GET Notes by ID
-notes.find(1005, (err, item) => {
-  if (err) {
-    console.error(err);
-  }
-  if (item) {
-    console.log(item);
-  } else {
-    console.log('not found');
-  }
-});
+// // GET Notes by ID
+// notes.find(1005, (err, item) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   if (item) {
+//     console.log(item);
+//   } else {
+//     console.log('not found');
+//   }
+// });
 
 // PUT (Update) Notes by ID
 const updateObj = {
--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 // TEMP: Simple In-Memory Database
 const data = require('./db/notes');
-//const simDB = require('./db/simDB');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 const { PORT } = require('./config');
 
 
@@ -12,32 +13,57 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.json());
 
 
 
-app.get('/api/notes/', (req, res) => {
-  const query = req.query;
-  const searchQuery = data.filter(function(item) {
-    if(item.title.includes(query.searchTerm)){
-      return item;
-    }else if(item.content.includes(query.searchTerm)){
-      return item;
-    } 
+
+app.get('/api/notes/', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(list);
   });
-  if(query.searchTerm === undefined){
-    res.json(data);
-  } else {
-    res.json(searchQuery);
-  }
-});
+
+}); 
+  
+  
+// const query = req.query;
+// const searchQuery = data.filter(function(item) {
+//   if(item.title.includes(query.searchTerm)){
+//     return item;
+//   }else if(item.content.includes(query.searchTerm)){
+//     return item;
+//   } 
+// });
+// if(query.searchTerm === undefined){
+//   res.json(data);
+// } else {
+//   res.json(searchQuery);
+// }
+
 
 
 app.get('/api/notes', (req, res) => {
   res.json(data);
 });
-app.get('/api/notes/:id', (req, res) =>{
-  const {id} = req.params;
-  res.json(data.find(item => item.id === Number(id)));
+app.get('/api/notes/:id', (req, res, next) =>{
+  const { id } = req.params;
+  //res.json(data.find(item => item.id === Number(id)));
+
+  notes.find(1005, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      console.log('not found');
+    }
+  });  
 });
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
